532e591751a910b46a024cdb4cee85b6911ad423
kyo88765 <kyo88765@gmail.com>
2017-03-12 23:46:57 +0900

    AutoBuild修正

--- AutoBuild/unity_build.sh
+++ AutoBuild/unity_build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+SCRIPT_PATH=$(cd $(dirname $0) && pwd)
+
 UNITY_PATH="/Applications/Unity/Unity.app/Contents/MacOS"
 
-PROJECT_PATH="../"
+PROJECT_PATH="$SCRIPT_PATH/.."
 EXECUTE_METHOD="BuildClass.Build"
 
 $UNITY_PATH/Unity -batchmode -quit \
