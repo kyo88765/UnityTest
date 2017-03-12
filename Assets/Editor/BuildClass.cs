7d5040a987699a1d6d04fa0a64f028aa2decb7ae
kyo88765 <kyo88765@gmail.com>
2017-03-12 15:16:22 +0900

    BuildClass修正

--- Assets/Editor/BuildClass.cs
+++ Assets/Editor/BuildClass.cs
@@ -1,21 +1,22 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
-public class BuildClass : MonoBehaviour 
+public class BuildClass : MonoBehaviour
 {
-	public static Build()
-	{
-		string[] sceneList =
-		{
-			"./Assets/Scene/setup.unity",
-			"./Assets/Scene/main.unity",
-		}
-	}
+    public static void Build()
+    {
+        string[] sceneList =
+        {
+            "./Assets/Scene/setup.unity",
+            "./Assets/Scene/main.unity",
+        };
 
-	string errorMessage = BuildPipeline.BuildPlayer
-	{
-		sceneList,
-		"/Users/kawaguchikyouhei/My/WorkSpace/Unity Projects/out/UnityTest.exe",
-		BuildTarget.StandaloneWindows,
-	}
+        string errorMessage = BuildPipeline.BuildPlayer
+        (
+            sceneList,
+            "/Users/kawaguchikyouhei/My/WorkSpace/Unity Projects/out/UnityTest.exe",
+            BuildTarget.StandaloneWindows,
+            BuildOptions.None
+        );
+    }
 }
