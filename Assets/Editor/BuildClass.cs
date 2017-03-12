fa617cdbaea9c6b7449a96e86b39e71e3a0ae68c
河口恭平 <kawaguchikyouhei@subkyoman.local>
2017-03-12 14:05:11 +0900

    BuildClass追加

---
+++ Assets/Editor/BuildClass.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BuildClass : MonoBehaviour 
+{
+	public static Build()
+	{
+		string[] sceneList =
+		{
+			"./Assets/Scene/setup.unity",
+			"./Assets/Scene/main.unity",
+		}
+	}
+
+	string errorMessage = BuildPipeline.BuildPlayer
+	{
+		sceneList,
+		"/Users/kawaguchikyouhei/My/WorkSpace/Unity Projects/out/UnityTest.exe",
+		BuildTarget.StandaloneWindows,
+	}
+}
