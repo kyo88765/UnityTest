3521b7f567e52162854947733932fe6cba7bb158
kyo88765 <kyo88765@gmail.com>
2017-03-14 00:06:05 +0900

    xcode_build.sh追加

---
+++ AutoBuild/xcode_build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SCRIPT_PATH=$(cd $(dirname $0) && pwd)
+
+PROJECT_PATH="$SCRIPT_PATH/../Build/Unity-iPhone.xcodeproj"
+SCHEME="Unity-iPhone"
+DESTINATION="platform=iOS,id=b32e2c78da6ed5a365e5dee5a458735d4819765f"
+
+xcodebuild \
+    -project $PROJECT_PATH \
+    -scheme $SCHEME \
+    -destination $DESTINATION \
+    -configuration Debug \
+    build
