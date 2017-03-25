5b76a57c0161c04f175eb5fa45b8a7c5a589a0ba
河口恭平 <kawaguchikyouhei@subkyoman.local>
2017-03-25 18:05:56 +0900

    ログ出力追加

---
+++ Assets/Scripts/System/LogOutPut.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+
+public class LogOutPut : MonoBehaviour 
+{
+	public string Condition;
+    public string StackTrace;
+    public LogType Type;
+    
+    private void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    private void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    private void HandleLog(string condition, string stackTrace, LogType type)
+    {
+        Condition   = condition;
+        StackTrace  = stackTrace;
+        Type        = type;
+    }
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(10, 10, 150, 100), Condition);
+	}
+}
